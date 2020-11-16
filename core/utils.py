b98eb558c647954ca20c3c9a0f20d3985ccd4268
yurii <jurakulek@gmail.com>
2020-11-16 22:37:08 +0200

    Added resizin util for image

--- core/utils.py
+++ core/utils.py
@@ -3,10 +3,14 @@ import shutil
 import random
 import string
 
-from fastapi import UploadFile
+from io import BytesIO
+from PIL import Image
+
+from fastapi import UploadFile, HTTPException
 from humps import camelize
 
-from core.settings import AWS_BUCKET_NAME, DEFAULT_MEDIA_FOLDER, S3_OBJECT_URL
+from core.settings import AWS_BUCKET_NAME, DEFAULT_MEDIA_FOLDER, S3_OBJECT_URL, \
+    IMAGE_MAX_WIDTH
 
 
 def to_camel(string: str) -> str:
@@ -22,6 +26,30 @@ def generate_unique_filename(filename):
     return f"{filename}_{''.join(random.choice(string.ascii_lowercase) for _ in range(6))}.{ext}"
 
 
+def resize_image(image):
+    """Resize image to max width and return it
+    """
+    try:
+        im = Image.open(image)
+        w, h = im.size
+        if w > IMAGE_MAX_WIDTH:
+            new_height = h / w * IMAGE_MAX_WIDTH
+            im = im.resize((int(IMAGE_MAX_WIDTH), int(new_height)), Image.ANTIALIAS)
+    except OSError as e:
+        raise HTTPException(status_code=400, detail=f"Wrong image format. {e}")
+
+    return im
+
+
+def bytes_from_image(image, img_format='jpeg'):
+    """Converts pillow image file to bytes"""
+    image = image.convert('RGB')
+    with BytesIO() as output:
+        image.save(output, format=img_format)
+        contents = output.getvalue()
+    return contents
+
+
 def upload_file(file: UploadFile):
     """Use this util for upload file
     If you are using S3, please make sure that you have set `AWS_BUCKET_NAME`
@@ -29,10 +57,11 @@ def upload_file(file: UploadFile):
     """
     filename = generate_unique_filename(file.filename)
     filepath = DEFAULT_MEDIA_FOLDER + filename
+    file = bytes_from_image(resize_image(file.file))
     if AWS_BUCKET_NAME:
         client = boto3.client('s3')
-        client.upload_fileobj(file.file, AWS_BUCKET_NAME, filepath)
+        client.upload_fileobj(BytesIO(file), AWS_BUCKET_NAME, filepath)
         return S3_OBJECT_URL + filepath
     with open(filepath, 'wb') as buffer:
-        shutil.copyfileobj(file.file, buffer)
+        buffer.write(file)
     return filepath
