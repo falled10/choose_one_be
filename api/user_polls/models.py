bab38a8e8524e6e6d07762412ab0af8dbfa9625c
yurii <jurakulek@gmail.com>
2020-11-30 15:41:26 +0200

    Added new migrations

--- api/user_polls/models.py
+++ api/user_polls/models.py
@@ -12,13 +12,13 @@ class UserPoll(Base):
     __tablename__ = 'user_polls'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey(User.id))
-    poll_id = Column(Integer, ForeignKey(Poll.id))
+    poll_id = Column(Integer, ForeignKey(Poll.id, ondelete='CASCADE'))
 
     created_at = Column(DateTime, default=datetime.utcnow)
 
     user = relationship(User, foreign_keys='UserPoll.user_id', back_populates="user_polls")
     poll = relationship(Poll, foreign_keys='UserPoll.poll_id', back_populates="user_polls")
-    user_options = relationship("UserOption", back_populates="poll")
+    user_options = relationship("UserOption", back_populates="poll", passive_deletes=True)
 
 
 class UserOption(Base):
